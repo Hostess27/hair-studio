3c1d8c5469c9ef06f2a16e96912359cee3700e5c
Hostess27 <68385172+Hostess27@users.noreply.github.com>
2022-10-01 22:50:22 +0000

    Deploying to gh-pages from @ Hostess27/hair-studio@e4a7419512cbfe3cf8b41109be37766e5ba38d14 🚀

---
+++ index.8b32a5f2.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("burger-menu"),t=document.getElementById("menu");e.addEventListener("click",(function(){this.classList.toggle("close"),t.classList.toggle("overlay")}))}();
+//# sourceMappingURL=index.8b32a5f2.js.map
---
+++ index.d3934462.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("burger-menu"),t=document.getElementById("menu");e.addEventListener("click",(function(){this.classList.toggle("close"),t.classList.toggle("overlay")}));
+//# sourceMappingURL=index.d3934462.js.map
